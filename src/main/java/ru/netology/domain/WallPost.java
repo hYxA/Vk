e495239e805a47f5a93da9fb2ab2136954251540
hYxA <zybu@live.ru>
2020-11-25 16:29:34 +0300

    update WallPost

--- src/main/java/ru/netology/domain/WallPost.java
+++ src/main/java/ru/netology/domain/WallPost.java
@@ -11,14 +11,23 @@ public class WallPost {
     private int ownerId;            // идентификатор владельца стены
     private int fromId;             // идентификатор автора записи
     private int createdBy;          // идентификатор администратора (для сообществ)
+    private int replyOwnerId;       // идентификатор владельца записи, в ответ на которую была оставлена текущая
+    private int replyPostId;        // идентификатор записи, в ответ на которую была оставлена текущая
+    private int postPonedId;        // идентификатор отложенной записи
+    private int views;              // количество просмотров
+
+    private boolean favourites;     // добавлено в избранное
+    private boolean accept;         // приватность. Может ли текущий пользователь видеть пост
+    private boolean canPin;         // может ли пользователь закрепить запись
+    private boolean canEdit;        // может ли пользователь редактировать запись
+    private boolean canDelete;      // может ли пользователь удалить запись
+    private boolean isPinned;       // пост закреплен
+    private boolean withAds;        // присутствует реклама
+
     private Date date;              // дата размещения записи............................возможно неправильный тип
     private Time time;              // время размещения записи...........................возможно неправильный тип
     private String body;            // основное тело поста со всеми картинками, ссылками и тд
-    private int replyOwnerId;       // идентификатор владельца записи, в ответ на которую была оставлена текущая
-    private int replyPostId;        // идентификатор записи, в ответ на которую была оставлена текущая
-    private boolean favourites;     // добавлено в избранное
-    private int views;              // количество просмотров
-    private boolean accept;         // приватность. Может ли текущий пользователь видеть пост
+
     private RepostInfo repostInfo;  // может ли текущий пользователь сделать репост этой записи
     private CommentsInfo commentsInfo;
     private LikesInfo likesInfo;
