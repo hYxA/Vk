507191ada663407bf4a0bab6c5655fb4744fa9d4
hYxA <zybu@live.ru>
2020-11-25 15:50:37 +0300

    added AttachmentsInfo

---
+++ src/main/java/ru/netology/domain/AttachmentsInfo.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class AttachmentsInfo {
+    /**
+     * нужно изучить конструктор переменных
+     * Может принимать значения: photo, audio, video, note, doc, graffiti, link, app, poll, page, album,
+     * photoList, market, marketAlbum, sticker, event, prettyCards
+     */
+    private String attachmentType;     // тип вложения
+
+
+}
--- src/main/java/ru/netology/domain/WallPost.java
+++ src/main/java/ru/netology/domain/WallPost.java
@@ -6,19 +6,20 @@ import java.util.Date;
 
 public class WallPost {
 
-    private int id;             // идентификатор поста
-    private int ownerId;        // идентификатор владельца стены
-    private int fromId;         // идентификатор автора записи
-    private int createdBy;      // идентификатор администратора (для сообществ)
-    private Date date;          // возможно неправильный тип
-    private Time time;          // возможно неправильный тип
-    private String body;        // основное тело поста со всеми картинками, ссылками и тд
-    private int replyOwnerId;   // идентификатор владельца записи, в ответ на которую была оставлена текущая
-    private int replyPostId;    // идентификатор записи, в ответ на которую была оставлена текущая
-    private boolean favourites; // добавлено в избранное
-    private int views;          // количество просмотров
-    private boolean accept;     // приватность. Может ли текущий пользователь видеть пост
+    private int id;                 // идентификатор поста
+    private int ownerId;            // идентификатор владельца стены
+    private int fromId;             // идентификатор автора записи
+    private int createdBy;          // идентификатор администратора (для сообществ)
+    private Date date;              // дата размещения записи............................возможно неправильный тип
+    private Time time;              // время размещения записи...........................возможно неправильный тип
+    private String body;            // основное тело поста со всеми картинками, ссылками и тд
+    private int replyOwnerId;       // идентификатор владельца записи, в ответ на которую была оставлена текущая
+    private int replyPostId;        // идентификатор записи, в ответ на которую была оставлена текущая
+    private boolean favourites;     // добавлено в избранное
+    private int views;              // количество просмотров
+    private boolean accept;         // приватность. Может ли текущий пользователь видеть пост
     private RepostInfo repostInfo;  // может ли текущий пользователь сделать репост этой записи
     private CommentsInfo commentsInfo;
     private LikesInfo likesInfo;
+    private AttachmentsInfo attachmentsInfo;
 }
