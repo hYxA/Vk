8a63142d4a393c7a1a4bbd035e332019885f608b
hYxA <zybu@live.ru>
2020-11-25 14:42:43 +0300

    added CommentsInfo

--- src/main/java/ru/netology/domain/WallPost.java
+++ src/main/java/ru/netology/domain/WallPost.java
@@ -7,14 +7,18 @@ import java.util.Date;
 public class WallPost {
 
     private int id;             // идентификатор поста
-    private ImageIcon avatar;   // возможно неправильный тип
-    private String nameOwner;   // owner потому что создатель поста мб как группа, так и страница (аккаунт)
+    private int ownerId;        // идентификатор владельца стены
+    private int fromId;         // идентификатор автора записи
+    private int createdBy;      // идентификатор администратора (для сообществ)
     private Date date;          // возможно неправильный тип
     private Time time;          // возможно неправильный тип
     private String body;        // основное тело поста со всеми картинками, ссылками и тд
+    private int replyOwnerId;   // идентификатор владельца записи, в ответ на которую была оставлена текущая
+    private int replyPostId;    // идентификатор записи, в ответ на которую была оставлена текущая
+    private int friendsOnly;    // метка "Только для друзей"
     private boolean favourites; // добавлено в избранное
     private int likes;          // количество лайков
-    private int comments;       // количество комментариев
     private int views;          // количество просмотров
     private boolean accept;     // приватность
+    private CommentsInfo commentsInfo;
 }
