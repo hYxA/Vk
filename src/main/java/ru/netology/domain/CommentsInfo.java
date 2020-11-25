31e47ef38044200c1f3f6a8daff5337458ccaf5f
hYxA <zybu@live.ru>
2020-11-25 15:13:08 +0300

    update CommentsInfo

--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -1,4 +1,9 @@
 package ru.netology.domain;
 
 public class CommentsInfo {
+    private int commentCounter;         // счётчик комментариев
+    private boolean canPost;            // может ли текущий пользователь публиковать комментарии
+    private boolean groupsCanPost;      // могут ли сообщества комментировать запись
+    private boolean canOpen;           // может ли текущий пользователь открыть комментарий к записи
+    private boolean canClose;           // может ли текущий пользователь закрыть комментарий к записи
 }
